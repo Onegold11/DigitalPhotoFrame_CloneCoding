618e1fa547990ba94887718c43066d613bf11e02
Onegold <ujini1129@gmail.com>
2021-08-30 17:51:48 +0900

    [feature] 사진 선택 기능 추가 - 권한 확인 기능 추가

--- app/src/main/java/com/clonecoding/digitalphotoframe/MainActivity.kt
+++ app/src/main/java/com/clonecoding/digitalphotoframe/MainActivity.kt
@@ -1,10 +1,16 @@
 package com.clonecoding.digitalphotoframe
 
+import android.app.Activity
+import android.content.Intent
 import android.content.pm.PackageManager
+import android.net.Uri
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AlertDialog
 import androidx.core.content.ContextCompat
@@ -12,6 +18,8 @@ import androidx.core.content.ContextCompat
 class MainActivity : AppCompatActivity() {
 
     private val READ_PERMISSION: String = android.Manifest.permission.READ_EXTERNAL_STORAGE
+
+    private val READ_STROAGE_CODE: Int = 100
 
     /**
      * Add photo button
@@ -26,6 +34,22 @@ class MainActivity : AppCompatActivity() {
     private val startPhotoFrameModeButton: Button by lazy {
         findViewById(R.id.startPhotoFrameModeButton)
     }
+
+    /**
+     * Image view List
+     */
+    private val imageViewList: List<ImageView> by lazy {
+        mutableListOf<ImageView>().apply {
+            add(findViewById(R.id.photoImageView1))
+            add(findViewById(R.id.photoImageView2))
+            add(findViewById(R.id.photoImageView3))
+            add(findViewById(R.id.photoImageView4))
+            add(findViewById(R.id.photoImageView5))
+            add(findViewById(R.id.photoImageView6))
+        }
+    }
+
+    private val imageUriList: MutableList<Uri> = mutableListOf()
 
     /**
      * onCreate
@@ -45,6 +69,13 @@ class MainActivity : AppCompatActivity() {
 
         this.startPhotoFrameModeButton.setOnClickListener {
 
+            val intent = Intent(this, PhotoFrameActivity::class.java)
+            this.imageUriList.forEachIndexed{ index, uri ->
+                intent.putExtra("photo$index", uri.toString())
+            }
+            intent.putExtra("photoListSize", imageUriList.size)
+
+            startActivity(intent)
         }
     }
 
@@ -59,7 +90,8 @@ class MainActivity : AppCompatActivity() {
                     this,
                     READ_PERMISSION
                 ) == PackageManager.PERMISSION_GRANTED -> {
-                    //TODO 사진 선택
+
+                    this.navigatePhotos()
                 }
                 shouldShowRequestPermissionRationale(
                     READ_PERMISSION
@@ -69,11 +101,13 @@ class MainActivity : AppCompatActivity() {
                 else -> {
                     requestPermissions(
                         arrayOf(READ_PERMISSION),
-                        100)
+                        READ_STROAGE_CODE)
                 }
             }
         }
     }
+
+
 
     /**
      * Show permission dialog
@@ -86,10 +120,75 @@ class MainActivity : AppCompatActivity() {
             .setPositiveButton("동의하기") { _, _ ->
                 requestPermissions(
                     arrayOf(READ_PERMISSION),
-                    100)
+                    READ_STROAGE_CODE)
             }
             .setNegativeButton("취소하기") {_, _ -> }
             .create()
             .show()
+    }
+
+    /**
+     * Permission result
+     */
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+
+        when (requestCode) {
+            READ_STROAGE_CODE -> {
+                if (grantResults.isEmpty() == false && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+                    this.navigatePhotos()
+                } else {
+                    Toast.makeText(this, "권한을 거부하셨습니다.", Toast.LENGTH_SHORT).show()
+                }
+            }
+            else -> {
+
+            }
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (resultCode != Activity.RESULT_OK) {
+            return
+        }
+
+        when(requestCode) {
+            200 -> {
+                val selectedImageUri: Uri? = data?.data
+
+                if (selectedImageUri != null) {
+
+                    if (imageUriList.size == 6) {
+                        Toast.makeText(this, "이미 사진이 꽉 찼습니다.", Toast.LENGTH_SHORT).show()
+                        return
+                    }
+
+                    this.imageUriList.add(selectedImageUri)
+                    this.imageViewList[imageUriList.size - 1].setImageURI(selectedImageUri)
+                } else {
+                    Toast.makeText(this, "사진을 가져오지 못했습니다.", Toast.LENGTH_SHORT).show()
+                }
+            }
+            else -> {
+                Toast.makeText(this, "사진을 가져오지 못했습니다.", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+    /**
+     * Navigate photos
+     */
+    private fun navigatePhotos() {
+
+        val intent = Intent(Intent.ACTION_GET_CONTENT)
+        intent.type = "image/*"
+        startActivityForResult(intent, 200)
     }
 }
